835cc1e3f196c504aebcf8789c35d72264c83d75
Anh Tuan <tanhtruong97@gmail.com>
2026-06-22 21:44:51 +0200

    PIG-144 add dimensional-dermoscopy shader spike (/dev/lesion-spike)

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -106,6 +106,11 @@ export const createAppRouter = (queryClient: QueryClient) =>
           lazy: () => import('./routes/dev-motion').then(convert(queryClient)),
         },
         {
+          path: '/dev/lesion-spike',
+          lazy: () =>
+            import('./routes/dev-lesion-spike').then(convert(queryClient)),
+        },
+        {
           path: '*',
           lazy: () => import('./routes/not-found').then(convert(queryClient)),
         },
